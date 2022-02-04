c5233b35b9abbc59615e0694cff6e8d768ab1c71
Tootouch <wogur379@gmail.com>
2022-02-04 10:02:19 +0900

    change path

--- DDP/distributed_train.sh
+++ DDP/distributed_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_PROC=$1
 shift
-python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC train.py "$@"
+python3 -m torch.distributed.launch --nproc_per_node=$NUM_PROC main.py "$@"
