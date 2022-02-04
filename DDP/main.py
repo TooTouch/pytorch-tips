12ef063fe9ca7585f21ae1f1e797d79be2b8841f
Tootouch <wogur379@gmail.com>
2022-02-04 10:31:03 +0900

    remove OrderedDistributedSampler

--- DDP/main.py
+++ DDP/main.py
@@ -6,12 +6,10 @@ import os
 import time
 import json
 import random
-import math
 import wandb
 import logging
 from collections import OrderedDict
 
-from torch.utils.data import Sampler
 import torch.distributed as dist
 
 import torch
@@ -59,52 +57,6 @@ def reduce_tensor(tensor, n):
     rt /= n
     return rt
 
-
-class OrderedDistributedSampler(Sampler):
-    """Sampler that restricts data loading to a subset of the dataset.
-    It is especially useful in conjunction with
-    :class:`torch.nn.parallel.DistributedDataParallel`. In such case, each
-    process can pass a DistributedSampler instance as a DataLoader sampler,
-    and load a subset of the original dataset that is exclusive to it.
-    .. note::
-        Dataset is assumed to be of constant size.
-    Arguments:
-        dataset: Dataset used for sampling.
-        num_replicas (optional): Number of processes participating in
-            distributed training.
-        rank (optional): Rank of the current process within num_replicas.
-    """
-
-    def __init__(self, dataset, num_replicas=None, rank=None):
-        if num_replicas is None:
-            if not dist.is_available():
-                raise RuntimeError("Requires distributed package to be available")
-            num_replicas = dist.get_world_size()
-        if rank is None:
-            if not dist.is_available():
-                raise RuntimeError("Requires distributed package to be available")
-            rank = dist.get_rank()
-        self.dataset = dataset
-        self.num_replicas = num_replicas
-        self.rank = rank
-        self.num_samples = int(math.ceil(len(self.dataset) * 1.0 / self.num_replicas))
-        self.total_size = self.num_samples * self.num_replicas
-
-    def __iter__(self):
-        indices = list(range(len(self.dataset)))
-
-        # add extra samples to make it evenly divisible
-        indices += indices[:(self.total_size - len(indices))]
-        assert len(indices) == self.total_size
-
-        # subsample
-        indices = indices[self.rank:self.total_size:self.num_replicas]
-        assert len(indices) == self.num_samples
-
-        return iter(indices)
-
-    def __len__(self):
-        return self.num_samples
 
 
 def train(model, dataloader, criterion, optimizer, log_interval, accumulation_steps=1, local_rank=0):   
